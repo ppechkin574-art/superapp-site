ba66af3b3efa5b37ccd0a13f2909450e55abb14c
MacBook Pro <macbookpro@MacBook-Pro-MacBook-Pro.local>
2026-04-19 00:04:12 +0500

    fix: add routes for superapp-app.html and superapp.html

--- website/server.js
+++ website/server.js
@@ -7,5 +7,7 @@ app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/app', (req, res) => res.sendFile(path.join(__dirname, 'app.html')));
+app.get('/superapp-app.html', (req, res) => res.sendFile(path.join(__dirname, 'app.html')));
+app.get('/superapp.html', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
