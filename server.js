767d59638dbc0ddf66a0e0cf0e017a4e0fbca70e
MacBook Pro <macbookpro@MacBook-Pro-MacBook-Pro.local>
2026-04-18 23:54:52 +0500

    Initial commit: SuperApp landing + interactive preview

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/app', (req, res) => res.sendFile(path.join(__dirname, 'app.html')));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
