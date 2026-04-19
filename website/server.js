bd221d3c0dd45991f624ad04991cb5b3a9c4ef82
MacBook Pro <macbookpro@MacBook-Pro-MacBook-Pro.local>
2026-04-19 11:14:55 +0500

    feat: add onboarding page with 3 screens

--- website/server.js
+++ website/server.js
@@ -9,5 +9,7 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/app', (req, res) => res.sendFile(path.join(__dirname, 'app.html')));
 app.get('/superapp-app.html', (req, res) => res.sendFile(path.join(__dirname, 'app.html')));
 app.get('/superapp.html', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/onboarding', (req, res) => res.sendFile(path.join(__dirname, 'onboarding.html')));
+app.get('/onboarding.html', (req, res) => res.sendFile(path.join(__dirname, 'onboarding.html')));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
